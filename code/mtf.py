d7102c3d7e0a72a82dd8303ae389f23209ab241d
Archer <shadow.cat6333@gmail.com>
2019-09-02 20:23:52 -0500

    Ecuaciones corregidas

--- code/mtf.py
+++ code/mtf.py
@@ -1,6 +1,5 @@
 import cv
 import math
-from random import randint
 
 def user_x(event,x,y,flags,param):
 	if event == cv.CV_EVENT_LBUTTONDOWN:
@@ -12,9 +11,9 @@ def user_x(event,x,y,flags,param):
 		user_distance = param[5]
 
 		snap = a*b*math.e**(b*x)
-		d = n_pixels/screen_size
-		alpha = math.atan(d/user_distance)*2.0*math.pi
-		f = 1.0/(snap*alpha)
+		d = screen_size/n_pixels
+		alpha = math.atan(d/user_distance)*(180.0/math.pi)
+		f = snap/alpha
 		print "Distance:%.3f, X:%.3f, Snap:%.3f, Alpha:%.3f, F:%.3f"%(
 			user_distance, x, snap, alpha, f)
 
@@ -24,7 +23,7 @@ def create_mtf(name, width, height, screen, distance_user):
 	b = math.log(width/2.0)/width
 	a = 1.0/(b*width)
 
-	d = 0.02
+	d = 0.01
 	c = math.log(d)/height
 
 	x_vector = [0.5+0.5*math.sin(2*math.pi* a*math.e**(b*val) ) for val in 
@@ -50,27 +49,16 @@ if __name__=="__main__":
 	create_mtf("out.png", size, size, screen_cm, distance_user_cm)
 
 
-#1 zapato sin lentes
-#Distance:30.000, X:315.000, Snap:0.018, Alpha:6.779, F:8.371
-#Distance:60.000, X:250.000, Snap:0.010, Alpha:4.718, F:20.364
-#Distance:100.000, X:325.000, Snap:0.019, Alpha:3.207, F:16.316
 
-#2 zapato con lentes
-#Distance:30.000, X:317.000, Snap:0.018, Alpha:6.779, F:8.236
-#Distance:60.000, X:367.000, Snap:0.027, Alpha:4.718, F:7.893
-#Distance:100.000, X:327.000, Snap:0.019, Alpha:3.207, F:16.054
+#Distance:30.000, X:513.000, Snap:0.088, Alpha:0.034, F:2.569
+#Distance:60.000, X:430.000, Snap:0.045, Alpha:0.017, F:2.623
+#Distance:100.000, X:359.000, Snap:0.025, Alpha:0.010, F:2.460
 
-#4
-#Distance:30.000, X:245.000, Snap:0.010, Alpha:6.779, F:14.758
-#Distance:60.000, X:250.000, Snap:0.010, Alpha:4.718, F:20.364
-#Distance:100.000, X:245.000, Snap:0.010, Alpha:3.207, F:31.193
+#Distance:30.000, X:627.000, Snap:0.221, Alpha:0.034, F:6.469
+#Distance:60.000, X:541.000, Snap:0.110, Alpha:0.017, F:6.446
+#Distance:100.000, X:475.000, Snap:0.064, Alpha:0.010, F:6.295
 
-#3 yo sin lentes
-#Distance:30.000, X:416.000, Snap:0.040, Alpha:6.779, F:3.694
-#Distance:60.000, X:409.000, Snap:0.038, Alpha:4.718, F:5.617
-#Distance:100.000, X:357.000, Snap:0.025, Alpha:3.207, F:12.590
+#Distance:30.000, X:498.000, Snap:0.078, Alpha:0.034, F:2.275
+#Distance:60.000, X:418.000, Snap:0.041, Alpha:0.017, F:2.380
+#Distance:100.000, X:358.000, Snap:0.025, Alpha:0.010, F:2.440
 
-#3 yo con lentes
-#Distance:30.000, X:375.000, Snap:0.029, Alpha:6.779, F:5.149
-#Distance:60.000, X:398.000, Snap:0.035, Alpha:4.718, F:6.140
-#Distance:100.000, X:403.000, Snap:0.036, Alpha:3.207, F:8.674
